cc5371a61025f342c4169d9f19091da53292bfd4
cajy78live <cajy78live@ip-172-31-78-209.ec2.internal>
2020-12-19 15:17:55 +0000

    Updated json

--- src/test/java/stepdef/TestRunner.java
+++ src/test/java/stepdef/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		monochrome = true,
-		plugin = {"pretty", "html:target/cucumber-html","json:target/cucumbeJson"},
+		plugin = {"pretty", "html:target/cucumberReport.html","json:target/cucumber.json"},
 		features = "src/test/java/features",
 		//tags = {"@Sanity"},
 		glue = { "stepdef" }
